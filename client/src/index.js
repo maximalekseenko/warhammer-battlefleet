33a882e3f3d5ad3588149e6996ab77a5ccf65437
maximalekseenko <max.alekseenko@gmail.com>
2023-09-20 18:09:09 +0300

    js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,14 @@
-require('nw.gui').Window.get().showDevTools();
+gui = require('nw.gui');
+gui.Window.get().showDevTools();
+
 LoadData();
 console.log(Data);
 
 jQuery(function() {
     OpenMenu("mainmenu.html");
 })
+
+function Quit()
+{
+    gui.App.quit();
+}
